05707718da511ad47a91e3538d34f5bee1dcaf5a
zeeeunee <wldmstv@gmail.com>
2024-01-26 10:55:45 +0900

    interface 지정

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ let odd = [1, 3, 5];
 let even = [2, 4, 6];
 //tuple: 배열의 들어갈 타입을 개별적으로 지정
 let combined = [3, 'homework', false];
+//유니온타입으로 들어갈수 있는 자료형을 복수개 지정
+let random = [4, '5', '3', 7];
+let student1 = {
+    name: 'David',
+    age: 20,
+    isFemale: false,
+    address: 'seoul',
+};
+let student2 = {
+    name: 'Emily',
+    age: 30,
+    isFemale: true,
+};
--- main.ts
+++ main.ts
@@ -10,3 +10,26 @@ let odd: number[] = [1, 3, 5];
 let even: Array<number> = [2, 4, 6];
 //tuple: 배열의 들어갈 타입을 개별적으로 지정
 let combined: [number, string, boolean] = [3, 'homework', false];
+//유니온타입으로 들어갈수 있는 자료형을 복수개 지정
+let random: Array<number | string> = [4, '5', '3', 7];
+
+//Interface : 객체같이 복잡한 구조의 타입을 지정을 할때
+//특정 경우에 대한 커스텀타입을 제작해서 재활용
+interface Student {
+	name: string;
+	age: number;
+	readonly isFemale: boolean; //특정 property를 수정불가하게 읽기 전용으로 지정
+	address?: string; //해당 property를 선택사항으로 지정
+}
+let student1: Student = {
+	name: 'David',
+	age: 20,
+	isFemale: false,
+	address: 'seoul',
+};
+
+let student2: Student = {
+	name: 'Emily',
+	age: 30,
+	isFemale: true,
+};
