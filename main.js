d0e9db2cad484fb2f7880616e7e4c3cb776a661f
zeeeunee <wldmstv@gmail.com>
2024-01-26 11:54:43 +0900

    기존의 property확장에는 interface가 편하기 때문에 가급적 객체의 타입은 인터페이스로 지정

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-//type: 특정 커스터마이징된 자료형을 새로 선언할때
-//type vs interface차이점
-//interface는 객체에만 적용가능
-//type은 자료형 상관없이 모두 적용가능
-//커스텀자료형을 만들어야 될 때 객체는 가급적 interface, 그 외 자료형은 type
 let student1 = {
-    name: 'Andy',
-    age: 40,
+    name: 'David',
+    age: 30,
     isFemale: false,
-    score: 'F',
 };
-student1.score = 'F';
--- main.ts
+++ main.ts
@@ -1,32 +1,17 @@
-//type: 특정 커스터마이징된 자료형을 새로 선언할때
-//type vs interface차이점
-//interface는 객체에만 적용가능
-//type은 자료형 상관없이 모두 적용가능
-//커스텀자료형을 만들어야 될 때 객체는 가급적 interface, 그 외 자료형은 type
-
-type Grade = 'A' | 'B' | 'C' | 'D' | 'F';
-
-/*
-interface Student {
-  name: string;
-  age: number;
-  readonly isFemale: boolean;
-  score: Grade;
-}
-*/
-//Interface 대신 type으로도 객체타입 선언가능
+//interface는 객체의 property 확장에 따른 구조변경이 편함
 type Student = {
 	name: string;
 	age: number;
-	readonly isFemale: boolean;
-	score: Grade;
+};
+
+type Student = {
+	name: string;
+	age: number;
+	isFemale: boolean;
 };
 
 let student1: Student = {
-	name: 'Andy',
-	age: 40,
+	name: 'David',
+	age: 30,
 	isFemale: false,
-	score: 'F',
 };
-
-student1.score = 'F';
