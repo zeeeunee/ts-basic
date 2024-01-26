f17dedec26570aca46cb34ee140f24dc5c4ea269
zeeeunee <wldmstv@gmail.com>
2024-01-26 11:47:34 +0900

    interface와 type의 차이

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const plus = (n1, n2) => {
-    console.log(n1);
-    console.log(n2);
+//type: 특정 커스터마이징된 자료형을 새로 선언할때
+//type vs interface차이점
+//interface는 객체에만 적용가능
+//type은 자료형 상관없이 모두 적용가능
+//커스텀자료형을 만들어야 될 때 객체는 가급적 interface, 그 외 자료형은 type
+let student1 = {
+    name: 'Andy',
+    age: 40,
+    isFemale: false,
+    score: 'F',
 };
-plus(2, 3, 4, 5);
-plus(1, 2);
+student1.score = 'F';
--- main.ts
+++ main.ts
@@ -1,12 +1,32 @@
-//파라미터로 전달되는 인수의 갯수를 특정할 수 없을때 rest파라미터 지정하고 배열타입 설정
-interface Calc {
-	(...n: number[]): number | void;
-}
+//type: 특정 커스터마이징된 자료형을 새로 선언할때
+//type vs interface차이점
+//interface는 객체에만 적용가능
+//type은 자료형 상관없이 모두 적용가능
+//커스텀자료형을 만들어야 될 때 객체는 가급적 interface, 그 외 자료형은 type
 
-const plus: Calc = (n1, n2) => {
-	console.log(n1);
-	console.log(n2);
+type Grade = 'A' | 'B' | 'C' | 'D' | 'F';
+
+/*
+interface Student {
+  name: string;
+  age: number;
+  readonly isFemale: boolean;
+  score: Grade;
+}
+*/
+//Interface 대신 type으로도 객체타입 선언가능
+type Student = {
+	name: string;
+	age: number;
+	readonly isFemale: boolean;
+	score: Grade;
 };
 
-plus(2, 3, 4, 5);
-plus(1, 2);
+let student1: Student = {
+	name: 'Andy',
+	age: 40,
+	isFemale: false,
+	score: 'F',
+};
+
+student1.score = 'F';
