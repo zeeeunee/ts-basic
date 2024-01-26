9ce3ee9025c812612a1cf43cb1cf365d60402268
zeeeunee <wldmstv@gmail.com>
2024-01-26 10:42:59 +0900

    기본, 배열타입 지정

---
+++ main.js
@@ -0,0 +1,11 @@
+//기본 타입
+let txt = 'hello';
+let num = 0;
+let isFemale = false;
+//참조형타입
+let likes = ['game', 'music', 'movie'];
+let unlikes = ['study', 'sports', 'homework'];
+let odd = [1, 3, 5];
+let even = [2, 4, 6];
+//tuple: 배열의 들어갈 타입을 개별적으로 지정
+let combined = [3, 'homework', false];
---
+++ main.ts
@@ -0,0 +1,12 @@
+//기본 타입
+let txt: string = 'hello';
+let num: number = 0;
+let isFemale: boolean = false;
+
+//참조형타입
+let likes: string[] = ['game', 'music', 'movie'];
+let unlikes: Array<string> = ['study', 'sports', 'homework'];
+let odd: number[] = [1, 3, 5];
+let even: Array<number> = [2, 4, 6];
+//tuple: 배열의 들어갈 타입을 개별적으로 지정
+let combined: [number, string, boolean] = [3, 'homework', false];
