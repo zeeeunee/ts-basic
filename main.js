caacda950f81dd33a7ae28d167a3ff9db45ff471
zeeeunee <wldmstv@gmail.com>
2024-01-26 12:40:42 +0900

    ||연산자보다 ??연산자를 써야되는 이유

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-//특정 파라미터값을 optional처리시 type의 오류는 넘어갈 수 있지만
-//optional처리한 값을 코드 내부적으로 예외처리하지 않으면 에러가 발생
-const test = (n1, n2, n3) => {
-    //const result = n1 + n2 + n3;
-    const result = n3 === undefined ? n1 + n2 : n1 + n2 + n3;
-    return result;
+// ||연산자로 예외처리했을때 문제가 발생할 수 있는 상황
+const introduce = (name, age) => {
+    console.log(`This is ${name}`);
+    console.log(`${name} is ${age !== null && age !== void 0 ? age : 'default'} years old`);
 };
-//위의 함수의 예외처리를 아래와 같이 간결하게 처리
-const test2 = (n1, n2, n3) => {
-    const result = n1 + n2 + (n3 || 0);
-    return result;
-};
-console.log(test2(1, 2));
+//기존 ||연산자보다 null병합 연산자를 써야되는 이유
+// ||연산자는 빈문자나 0같이 실제적인 값조차도 false로 인식해서 예외처리하므로 예상치못한 문제발생 가능
+// ??연산자는 무조건 undefined, null같이 실제적으로 에러가 발생할만한 상황에서만 예외처리
+introduce('my Baby', 0);
