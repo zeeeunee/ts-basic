5480af173431b23fc43d64120d7280c04acd5993
zeeeunee <wldmstv@gmail.com>
2024-01-26 12:04:21 +0900

    type을 써야되는 구체적인 사례

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+//type 주로 쓰는 경우
+//1. 기존 객체타입의 property의 추가가 아닌, 서로 다른 객체를 합쳐서 새로운 타입을 만들때
+//2. 객체가 아닌 일반 자료형인데
 let student1 = {
-    name: 'David',
+    name: 'Emily',
     age: 30,
-    isFemale: false,
+    isFemale: true,
+};
+const test = (info) => {
+    console.log(info);
 };
--- main.ts
+++ main.ts
@@ -1,17 +1,29 @@
-//interface는 객체의 property 확장에 따른 구조변경이 편함
-type Student = {
-	name: string;
-	age: number;
-};
+//type 주로 쓰는 경우
+//1. 기존 객체타입의 property의 추가가 아닌, 서로 다른 객체를 합쳐서 새로운 타입을 만들때
+//2. 객체가 아닌 일반 자료형인데
 
-type Student = {
+interface StudentA {
 	name: string;
 	age: number;
+}
+
+interface StudentB {
+	name: string;
 	isFemale: boolean;
+}
+
+//type이용해서 두개의 인터페이스에서 Intersection으로 &&로 합쳐서 새로타입지정
+type StudentTotal = StudentA & StudentB;
+
+let student1: StudentTotal = {
+	name: 'Emily',
+	age: 30,
+	isFemale: true,
 };
 
-let student1: Student = {
-	name: 'David',
-	age: 30,
-	isFemale: false,
+//객체는 아니지만 특정 커스터마이징된 타입을 재활용해야 될떄 type으로 지정
+type commonType = string | null;
+
+const test = (info: commonType) => {
+	console.log(info);
 };
