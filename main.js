0d18c2349c97aed0d0adbd752d99a08c2a944cb9
zeeeunee <wldmstv@gmail.com>
2024-01-26 11:17:22 +0900

    함수도 인터페이스로 타입 재활용 가능

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-//리턴값이 있는 함수면 파라미터뒤에 리턴값의 타입지정가능
-//리턴값이 없으면 리턴값의 타입을 void지정
-//일반적으로 인수가 제대로 전달되면 리턴값이 잘못나올리가 없고
-//리턴으로 에러가 잡혀봤자 이미 에러가 난 상황이기 때문에 return에 타입지정이 무의미
 const plus = (n1, n2) => {
-    //console.log(n1 + n2);
     return n1 + n2;
 };
 const minus = (n1, n2) => {
     return n1 - n2;
 };
 const multiply = (n1, n2) => {
-    return n1 * n2;
+    console.log(n1 * n2);
 };
 const divider = (n1, n2) => {
     return n1 / n2;
--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
-//리턴값이 있는 함수면 파라미터뒤에 리턴값의 타입지정가능
-//리턴값이 없으면 리턴값의 타입을 void지정
-//일반적으로 인수가 제대로 전달되면 리턴값이 잘못나올리가 없고
-//리턴으로 에러가 잡혀봤자 이미 에러가 난 상황이기 때문에 return에 타입지정이 무의미
-const plus = (n1: number, n2: number): number => {
-	//console.log(n1 + n2);
+//다른 로직의 함수라고 하더라도 전달되는 인수와 리턴값이 매번 같은 패턴이면 일일이 함수마다 타입지정이 번거로움
+//함수로 인터페이스로 특정함수의 타입을 지정해서 재활용가능
+interface Calc {
+	(n1: number, n2: number): number | void;
+}
+
+const plus: Calc = (n1, n2) => {
 	return n1 + n2;
 };
 
-const minus = (n1: number, n2: number) => {
+const minus: Calc = (n1, n2) => {
 	return n1 - n2;
 };
 
-const multiply = (n1: number, n2: number) => {
-	return n1 * n2;
+const multiply: Calc = (n1, n2) => {
+	console.log(n1 * n2);
 };
 
-const divider = (n1: number, n2: number) => {
+const divider: Calc = (n1, n2) => {
 	return n1 / n2;
 };
